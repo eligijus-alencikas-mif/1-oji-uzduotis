e18f41f00d01f73fcbc51e18b7b830d7b2c7b40d
eli <eligijus.alencikas@mif.stud.vu.lt>
2025-02-09 23:37:33 +0200

    Sukurtas header failas

--- main.cpp
+++ main.cpp
@@ -1,14 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <algorithm>
-
-#define NAME_LENGTH 15
-#define HW_WEIGHT 0.4
-#define EXAM_WEIGHT 0.6
-
-using std::cin;
-using std::cout;
+#include "main.h";
 
 struct Student
 {
---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+#define NAME_LENGTH 15
+#define HW_WEIGHT 0.4
+#define EXAM_WEIGHT 0.6
+
+using std::cin;
+using std::cout;
+
+#endif //MAIN_H
