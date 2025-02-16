cc85b5d1f9cd29e7d42bb8b6754af706356a673b
eli <eligijus.alencikas@mif.stud.vu.lt>
2025-02-16 12:07:37 +0200

    use_c_arr.cpp is no longer needed

--- main.cpp
+++ main.cpp
@@ -14,15 +14,10 @@ int main() {
         return 0;
     }
 
-    choice = numInput("Pasirinkite kodo versija (1 - naudojant tik vektorius, 2 - naudojant c masyva namu darbams saugoti): ");
-    bool use_vec_ver = choice == 1;
     choice = numInput("Pasirinkite galutinio balo skaiciavimo buda (1 - vidurkis, 2 - mediana): ", 2, 1);
     bool use_median = choice != 1;
 
-    if (use_vec_ver) {
-        use_vector(use_median, generate_names, generate_grades);
-    }else {
-        use_c_arr(use_median, generate_names, generate_grades);
-    }
+    use_vector(use_median, generate_names, generate_grades);
+
     return 0;
 }
