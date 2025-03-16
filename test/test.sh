e879b3945369ccba1a4808d7d0f7f450501c8524
Eligijus Alencikas <eligijus.alencikas@mif.stud.vu.lt>
2025-03-16 19:16:03 +0200

    cleanup

--- test/test.sh
+++ test/test.sh
@@ -1,35 +1,36 @@
 > ./test_res.txt
 for i in {1..5}
 do
-../build/1-oji-uzduotis < ./1000t.txt
+../build/1-oji-uzduotis < ./test_cases/1000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
 echo "-" >> ./test_res.txt
 for i in {1..5}
 do
-../build/1-oji-uzduotis < ./10000t.txt
+../build/1-oji-uzduotis < ./test_cases/10000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
 echo "-" >> ./test_res.txt
 for i in {1..5}
 do
-../build/1-oji-uzduotis < ./100000t.txt
+../build/1-oji-uzduotis < ./test_cases/100000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
 echo "-" >> ./test_res.txt
 for i in {1..5}
 do
-../build/1-oji-uzduotis < ./1000000t.txt
+../build/1-oji-uzduotis < ./test_cases/1000000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
 echo "-" >> ./test_res.txt
 for i in {1..5}
 do
-../build/1-oji-uzduotis < ./10000000t.txt
+../build/1-oji-uzduotis < ./test_cases/10000000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+rm ./out.txt ./laikai.txt ./nuskriaustukai.txt ./galvociai.txt
