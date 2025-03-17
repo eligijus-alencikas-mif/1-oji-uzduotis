b360e760de117087382ecc81587f11864a4dd01a
Eligijus Alencikas <eligijus.alencikas@mif.stud.vu.lt>
2025-03-17 13:07:53 +0200

    testing now works as intended

--- test/test.sh
+++ test/test.sh
@@ -5,29 +5,38 @@ do
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+echo "-" >> ./test_res.txt
+
 for i in {1..5}
 do
 ../build/1-oji-uzduotis < ./test_cases/10000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+echo "-" >> ./test_res.txt
+
 for i in {1..5}
 do
 ../build/1-oji-uzduotis < ./test_cases/100000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+echo "-" >> ./test_res.txt
+
 for i in {1..5}
 do
 ../build/1-oji-uzduotis < ./test_cases/1000000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+echo "-" >> ./test_res.txt
+
 for i in {1..5}
 do
 ../build/1-oji-uzduotis < ./test_cases/10000000t.txt
 cat ./laikai.txt >> ./test_res.txt
 echo  >> ./test_res.txt
 done
+
 rm ./out.txt ./laikai.txt ./nuskriaustukai.txt ./galvociai.txt
-./time-table
+./time-table μs
